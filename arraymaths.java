78ebf5f15c0269e2b3177833e48cc2ae0a4eaa63
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 16:58:33 +0530

    Add files via upload

---
+++ arraymaths.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+class arraymaths 
+{
+    public  void main()
+    {
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("please enter the size of the array");
+    int size = sc.nextInt();
+        
+        float arr[]= new int[size];
+        int i,j;
+        for(i=0;i<size+1;i++)
+        {
+            System.out.println("please enter the values in the array ");
+            arr[i]=sc.nextInt();
+            
+            int var = Math.sqrt(arr[i]);
+        }
+    
+    
+    for(j=0; j<i; j++)
+    {
+        System.out.println("array original elements are ="+ arr[j]);
+    }*/
+    
+    //practice problem 1 
+    /*float  arr[] = { 45.7f,67.8f,63.4f,99.2f,100.0f};
+    float sum =0;
+    int i;
+    for(i=0;i<5;i++)
+    {
+        sum = sum + arr[i];
+    }
+    System.out.println("the value of sum is ="+sum);*/
+    //practice problem 2
+    float  arr[] = { 45.7f,67.8f,63.4f,99.2f,100.0f};
+    float sum =0;
+    int i;
+    for(i=0;i<5;i++)
+    {
+        sum = sum + arr[i];
+    }
+    System.out.println("the value of sum is ="+sum);
+}
+}
+    
+        
